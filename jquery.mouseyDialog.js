84ce4f5834f70b24475b309f726a80f7a1d0e116
Mat Biscan <mdbiscan@gmail.com>
2010-04-30 17:42:58 -0400

    refactored the big bugs

--- jquery.mouseyDialog.js
+++ jquery.mouseyDialog.js
@@ -32,111 +32,100 @@ jQuery simpleCarousel Plugin
     var settings = $.extend({}, $.fn.mouseyDialog.defaults, options); 
     
     return this.each(function() {
-      $anchor = $(this);
-      $dialog = $($anchor.attr('href'));
-      $closeButton = $('<a href="#" class="mouseyDialog_close">close</a>');
-      $zIndex = settings.zIndex;
-      $addOffset = settings.addOffset;
-      $animation = settings.animation;
-      $animationSpeed = settings.animationSpeed;
-      $draggable = settings.draggable;
+      var $anchor = $(this);
+      var $dialog = $($anchor.attr('href'));
+      var $closeButton = $('<a href="#" class="mouseyDialog_close">close</a>');
 
-      setup();
-      bindEvents();
-    });
-  };
-  
-  // Private functions
-  function setup() {
-    $closeButton
-      .appendTo($dialog);
-      
-    $dialog
-      .hide()
-      .css({position:'absolute', zIndex:$zIndex})
-      .addClass('mouseyDialog')
-      .appendTo('body');
-      
-    if($draggable) {
-      $dialog.draggable();
-    }
-  };
-  
-  function bindEvents() {
-    // Custom event
-    $anchor.bind('toggleDialog', function(event, x, y) {
-      if($dialog.hasClass('visible')) {
-        closeDialog($dialog);
-      } else {
-        openDialog($dialog, x, y);
-      }
-    });
+      // Setup
+     $closeButton
+        .appendTo($dialog);
 
-    // Events
-    $anchor.click(function(mouse) {
-      var windowWidth = $(window).width();
-      var windowHeight = $(window).height();
-      
-      var dialogWidth = $dialog.innerWidth();
-      var dialogHeight = $dialog.height() + $dialog.innerHeight();
-      
-      var browserHeight = mouse.screenY;
-      var browserWidth = mouse.screenX;
-      
-      var browserX = browserWidth+dialogWidth;
-      var browserY = browserHeight+dialogHeight;
-      
-      if(browserX >= windowWidth) {
-        var x = mouse.pageX-$addOffset-(dialogWidth);
-      } else {
-        var x = mouse.pageX+$addOffset;
-      }
-      if(browserY >= windowHeight) {
-        var y = mouse.pageY-$addOffset-(dialogHeight);
-      } else {
-        var y = mouse.pageY+$addOffset;
-      }
-      $(this).trigger('toggleDialog', [x, y]);
+      $dialog
+        .hide()
+        .css({position:'absolute', zIndex:settings.zIndex})
+        .addClass('mouseyDialog')
+        .appendTo('body');
 
-      var openDialog = $('.mouseyDialog.visible');
-      if(openDialog.length == 1 && openDialog != $dialog) {
-        closeDialog(openDialog);
+      if(settings.draggable) {
+        $dialog.draggable();
       }
-      return false;
-    });
-    
-    $closeButton.click(function() {
-      $anchor.trigger('toggleDialog');
-      return false; 
-    });
-
-    // Prevents the dialog from being closed when clicking inside it
-    $dialog.click(function(event) {
-      event.stopPropagation();
-    });
-    // Closes the dialog when clicking outside of it
-    $(document).click(function(event) {
-      if(event.target != this) {
+      
+      // Events
+      // Custom event
+      $anchor.bind('toggleDialog', function(event, x, y) {
         if($dialog.hasClass('visible')) {
           closeDialog($dialog);
+        } else {
+          openDialog($dialog, x, y);
         }
-      } 
-    });
-  };
-  
-  function openDialog(dialog, x, y) {
-    var animation = ($animation == 'slide' ? 'slideDown' : 'fadeIn');
+      });
 
-    $(dialog).css({top:y, left:x})[animation]($animationSpeed, function() {
-      $(this).addClass('visible');
-    });
-  };
-  
-  function closeDialog(dialog) {
-    var animation = ($animation == 'slide' ? 'slideUp' : 'fadeOut');
+      // Events
+      $anchor.click(function(mouse) {
+        var windowWidth = $(window).width();
+        var windowHeight = $(window).height();
 
-    $(dialog)[animation]($animationSpeed, function() {
-      $(this).removeClass('visible');
+        var dialogWidth = $dialog.innerWidth();
+        var dialogHeight = $dialog.height() + $dialog.innerHeight();
+
+        var browserHeight = mouse.screenY;
+        var browserWidth = mouse.screenX;
+
+        var browserX = browserWidth+dialogWidth;
+        var browserY = browserHeight+dialogHeight;
+
+        if(browserX >= windowWidth) {
+          var x = mouse.pageX-settings.addOffset-(dialogWidth);
+        } else {
+          var x = mouse.pageX+settings.addOffset;
+        }
+        if(browserY >= windowHeight) {
+          var y = mouse.pageY-settings.addOffset-(dialogHeight);
+        } else {
+          var y = mouse.pageY+settings.addOffset;
+        }
+        $(this).trigger('toggleDialog', [x, y]);
+
+        var openDialog = $('.mouseyDialog.visible');
+        if(openDialog.length == 1 && openDialog != $dialog) {
+          closeDialog(openDialog);
+        }
+        return false;
+      });
+
+      $closeButton.click(function() {
+        $anchor.trigger('toggleDialog');
+        return false; 
+      });
+
+      // Prevents the dialog from being closed when clicking inside it
+      $dialog.click(function(event) {
+        event.stopPropagation();
+      });
+      // Closes the dialog when clicking outside of it
+      $(document).click(function(event) {
+        if(event.target != this) {
+          if($dialog.hasClass('visible')) {
+            closeDialog($dialog);
+          }
+        } 
+      });
+      
+      function openDialog(dialog, x, y) {
+        var animation = (settings.animation == 'slide' ? 'slideDown' : 'fadeIn');
+
+        $(dialog).css({top:y, left:x})[animation](settings.animationSpeed, function() {
+          $(this).addClass('visible');
+        });
+      };
+
+      function closeDialog(dialog) {
+        var animation = (settings.animation == 'slide' ? 'slideUp' : 'fadeOut');
+
+        $(dialog)[animation](settings.animationSpeed, function() {
+          $(this).removeClass('visible');
+        });
+      };
     });
   };
   
--- prototype_pattern.js
+++ prototype_pattern.js
@@ -1,13 +1,31 @@
 /*
-jQuery mouseyDialog Plugin
+jQuery simpleCarousel Plugin
   * Version 1.0
-  * 2009-03-22 19:30:05
-  * URL: http://github.com/mdbiscan/mouseyDialog
-  * Description: jQuery mouseyDialog Plugin makes dialogs easy
+  * 04-30-2010
+  * URL: http://github.com/mdbiscan/simpleCarousel
   * Author: M.Biscan
-  * Copyright: Copyright (c) 2010 M.Biscan
-  * Licence: dual, MIT/GPLv2
-  * requires jQuery1.4.2 and jQueryUI 1.8
+  * requires jQuery1.4.2 & jQueryUI 1.8 (for draggable())
+  
+  Copyright (c) 2010 M.Biscan
+
+  Permission is hereby granted, free of charge, to any person obtaining
+  a copy of this software and associated documentation files (the
+  "Software"), to deal in the Software without restriction, including
+  without limitation the rights to use, copy, modify, merge, publish,
+  distribute, sublicense, and/or sell copies of the Software, and to
+  permit persons to whom the Software is furnished to do so, subject to
+  the following conditions:
+
+  The above copyright notice and this permission notice shall be
+  included in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 (function($){
   jQuery.fn.mouseyDialog = function(options) {
